fd08f42fdcd05b1ea64cf7768b6ff61e895b5bdf
Magnus Øverbø <magnus@maggz.nl>
2015-08-10 12:08:43 +0200

    Update getImg.php
    Added progress data as part of retrieving image

--- getImg.php
+++ getImg.php
@@ -1,17 +1,24 @@
 <?php
-	$conn = new mysqli('localhost', 'root', 'toor', 'webIIC');
-	$bdir = "/development/dbIris/img_processed/";
+	$sqlImage = "SELECT * FROM image WHERE CLASS = -1 LIMIT 1";
+	$sqlProg  = "SELECT (SELECT COUNT(AID) FROM image WHERE CLASS=1)  AS GOOD, " .
+			  " (SELECT COUNT(AID) FROM image WHERE CLASS=0)  AS BAD   " .
+			  " (SELECT COUNT(AID) FROM image WHERE CLASS=-1) AS REM   FROM image";
 
-	$sql 	= "SELECT * FROM image WHERE CLASS = -1 LIMIT 1";
-	$res 	= $conn->query( $sql );
-	$R 		= $res->fetch_assoc( );
-
+	$conn	= new mysqli('localhost', 'root', 'toor', 'webIIC');
+	$bdir	= "/development/dbIris/img_processed/";
+	$res	= $conn->query( $sqlImage );
+	$R 	= $res->fetch_assoc( );
+	$res 	= $conn->query(  );
+	$S 	= $res->fetch_assoc( );
 	if( $R != false) {
-		echo '{	"AID":"' . $R['AID']  . '", ' .
-				 '  "SEG":"' . $R['PATH'] . "_segm.bmp" . '",' .
-				 '  "MSK":"' . $R['PATH'] . "_mask.bmp" . '",' .
-				 '  "IMN":"' . $R['PATH'] . "_imno.bmp" . '",' .
-				 '  "MAN":"' . $R['PATH'] . "_mano.bmp" . '"' .
-				 '}';
+		echo 	'{ "AID":"' . $R['AID']  . '", ' .
+			'  "GOD":"' . $S['GOOD'] . '", ' .
+			'  "BAD":"' . $S['BAD']  . '", ' .
+			'  "REM":"' . $S['REM']  . '", ' .
+			'  "SEG":"' . $R['PATH'] . "_segm.bmp" . '",' .
+			'  "MSK":"' . $R['PATH'] . "_mask.bmp" . '",' .
+			'  "IMN":"' . $R['PATH'] . "_imno.bmp" . '",' .
+			'  "MAN":"' . $R['PATH'] . "_mano.bmp" . '"'  .
+			'}';
 	}
 ?>
