bcf4fabb36014e669090cced93c21268c17eef35
Magnus Øverbø <magnus@maggz.nl>
2015-12-04 00:53:54 +0100

    Added functionality to revisit Manually created images

--- getImg.php
+++ getImg.php
@@ -1,32 +1,30 @@
 <?php
 	$bdir	= "/dbIris/img_processed/";
-	
-	$sqlProg	= "SELECT (SELECT COUNT(AID) FROM image WHERE CLASS=1) AS GOOD, " .
-							"(SELECT COUNT(AID) FROM image WHERE CLASS=0) AS BAD,  " .
-							"(SELECT COUNT(AID) FROM image WHERE CLASS=-1) AS REM,  " .
-							"(SELECT COUNT(AID) FROM image WHERE CLASS NOT IN (1,0,-1)) AS ODD";
-
 
 	$conn	= new mysqli('localhost', 'root', 'toor', 'webIIC');
 	
+	$sqlProg	= "SELECT  (SELECT COUNT(AID) FROM image WHERE CLASS= 1) AS GOOD, " .
+											"(SELECT COUNT(AID) FROM image WHERE CLASS= 0) AS BAD,  " .
+											"(SELECT COUNT(AID) FROM image WHERE CLASS=-1) AS REM,  " .
+											"(SELECT COUNT(AID) FROM image WHERE CLASS NOT IN (1,0,-1)) AS ODD";
 	$res 	= $conn->query( $sqlProg );
 	$S 	= $res->fetch_assoc( );
-	$I 	= intval( $S['REM'] );
-	$X	= rand(1, $I);
 
-	$sqlImage = "SELECT * FROM image WHERE CLASS=-1 AND STATUS=1 LIMIT $X, 1";
+	//$sqlImage = "SELECT AID FROM image WHERE CLASS=-1";
+	$sqlImage = "SELECT AID FROM image WHERE COORD!='' AND CLASS=-2";
+	$res	= $conn->query( $sqlImage );
+	$ID		= rand(0, $res->num_rows);
 
+	$sqlImage = "SELECT * FROM image WHERE AID=$ID LIMIT 1";
 	$res	= $conn->query( $sqlImage );
 	$R 	= $res->fetch_assoc( );
 
 	if( $R != false) {
 		echo 	'{ "AID":"' . $R['AID']  . '", ' .
-
 			'  "GOD":"' . $S['GOOD'] . '", ' .
 			'  "BAD":"' . $S['BAD']  . '", ' .
 			'  "REM":"' . $S['REM']  . '", ' .
 			'  "ODD":"' . $S['ODD']  . '", ' .
-
 			'  "CLS":"' . $R['CLASS']. '", ' .
 			'  "ORG":"' . $R[ 'ORG' ]. '", ' .
 			//'  "IMN":"' . $R['PATH'] . "_imno.bmp" . '",' .
@@ -34,5 +32,4 @@
 			'  "SEG":"' . $R['PATH'] . "_segm.bmp" . '",' .
 			'  "MSK":"' . $R['PATH'] . "_mask.bmp" . '"' .
 			'}';
-	}
-?>
+	}?>
